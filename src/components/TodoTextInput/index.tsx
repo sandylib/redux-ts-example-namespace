39c0cbe216009f68ea450f72c8be3e2d80eaaf4f
Sandy Zhang <sandy.zhang@revenue.nsw.gov.au>
2020-12-10 12:04:39 +1100

    react redux typescript boilerplate

---
+++ src/components/TodoTextInput/index.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import classNames from 'classnames';
+import style from './style.css';
+
+export declare namespace TodoTextInput {
+  export interface Props {
+    placeholder?: string;
+    newTodo?: boolean;
+    onSave: (text: string) => void;
+  }
+
+  export interface State {
+    text: string;
+  }
+}
+
+export const TodoTextInput = ({ placeholder, newTodo, onSave }: TodoTextInput.Props): JSX.Element => {
+  const [inputText, setInputText] = useState('');
+
+  const handleSubmit = React.useCallback(
+    (event: React.KeyboardEvent<HTMLInputElement>) => {
+      const text = event.currentTarget.value.trim();
+      if (event.which === 13) {
+        onSave(text);
+        setInputText('');
+      }
+    },
+    [onSave, setInputText]
+  );
+
+  const handleChange = React.useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setInputText(event.target.value);
+    },
+    [setInputText]
+  );
+
+  const handleBlur = React.useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const text = event.currentTarget.value.trim();
+      if (!newTodo) {
+        onSave(text);
+      }
+    },
+    [onSave]
+  );
+
+  const classes = classNames(
+    {
+      [style.edit]: !newTodo,
+      [style.new]: newTodo
+    },
+    style.normal
+  );
+
+  return (
+    /* eslint-disable */
+    <input
+      className={classes}
+      type="text"
+      autoFocus={true}
+      placeholder={placeholder}
+      value={inputText}
+      onChange={handleChange}
+      onBlur={handleBlur}
+      onKeyDown={handleSubmit}
+    />
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createBrowserHistory } from 'history';
+import { Router } from 'react-router';
+import { Route, Switch } from 'react-router';
+import { Provider } from 'react-redux';
+import { configureStore } from './store';
+import {App as TodoApp} from './example/Todo'
 import Screens from './screens';
+
+
+const history = createBrowserHistory();
+const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <Screens />
+    <Provider store={store}>
+     <Router history={history}>
+        <Switch>
+            <Route path="/todo" component={TodoApp}/> 
+             
+            <Route path="/">
+              <Screens />
+            </Route>
+          </Switch>
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
