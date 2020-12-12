960dab122f8bcd854842f19da23152ae9b594f69
Sandy Zhang <sandy.zhang@revenue.nsw.gov.au>
2020-12-12 15:46:08 +1100

    add dumy in the middleware

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,8 +1,17 @@
 import { Middleware } from 'redux';
+import { TodoActions } from '../actions/todos';
 
 export const logger: Middleware = (store) => (next) => (action) => {
   if (process.env.NODE_ENV !== 'production') {
     console.log(action);
   }
+
+  if(action.type === TodoActions.Type.ADD_TODO) {
+    store.dispatch({type:'do somthing', payload:'what ever'})
+  }
+  
+
+  // store.dispatch({type:TodoActions.Type.ADD_TODO, payload:'what ever'})
+
   return next(action);
 };
